2d8242557d29e77559b720fdd12eced2b04e41ff
Gustavo Alves <gustavo_alves@outlook.com>
2020-06-06 17:14:40 -0300

    Using annotations with JUnit: BeforeAll, AfterAll, BeforeEach, AfterEach

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,32 +1,68 @@
 package com.openclassrooms.testing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
-    @Test public void shouldAddTwoPositiveNumbers() {
+    private static Instant startedAt;
+    private Calculator calculator;
+
+    @BeforeAll
+    public static void beforeClass() {
+        startedAt = Instant.now();
+
+        System.out.println("Called only ONCE BEFORE ALL tests at " + startedAt);
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        Instant endedAt = Instant.now();
+        Duration duration = Duration.between(startedAt, endedAt);
+
+        System.out.println("Called only ONCE AFTER ALL tests - " + duration);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+
+        System.out.println("Called BEFORE EACH test - " + Instant.now());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null;
+
+        System.out.println("Called AFTER EACH test - " + Instant.now());
+    }
+
+    @Test
+    public void shouldAddTwoPositiveNumbers() {
         // arrange
         Integer expected = 3;
-        Calculator calculator = new Calculator();
 
         // act
         Integer sum = calculator.add(1, 2);
 
         // assert
-        assertEquals(expected, sum);
+        // assertEquals(expected, sum);
+        assertThat(sum, is(expected));
     }
 
-    @Test public void shouldMultiplyTwoPositiveNumbers() {
+    @Test
+    public void shouldMultiplyTwoPositiveNumbers() {
         Integer expected = 15;
-        Calculator calculator = new Calculator();
 
         Integer product = calculator.times(3, 5);
 
         // assertEquals(expected, product);
         assertThat(product, is(expected));
     }
+
 }
